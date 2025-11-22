653c4dd92a59c5edc249f6075b6532a9096ff724
flowprimedesign <luisibarradesign@gmail.com>
2025-11-22 09:06:03 -0800

    fix: render blackhole.gif via <img> instead of <video>

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -4,14 +4,11 @@ import HeroContent from "../sub/HeroContent";
 const Hero = () => {
   return (
     <div className="relative flex flex-col min-h-screen w-full">
-      <video
-        autoPlay
-        muted
-        loop
+      <img
+        src="/blackhole.gif"
+        alt="Animated blackhole background"
         className="rotate-180 absolute top-[-340px] left-0 z-[0] w-full h-full object-cover opacity-60"
-      >
-        <source src="/blackhole.gif" type="video/gif" />
-      </video>
+      />
       <HeroContent />
     </div>
   );
